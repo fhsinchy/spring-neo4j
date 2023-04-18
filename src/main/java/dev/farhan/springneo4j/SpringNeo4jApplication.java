0da662c6521f7cd84ed7d98ae9639e33d5abdf83
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-04-18 23:01:32 +0600

    Initial commit

---
+++ src/main/java/dev/farhan/springneo4j/SpringNeo4jApplication.java
@@ -0,0 +1,13 @@
+package dev.farhan.springneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNeo4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/farhan/springneo4j/SpringNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farhan.springneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
