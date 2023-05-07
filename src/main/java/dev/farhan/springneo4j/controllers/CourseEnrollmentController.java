b79c6023dc7fc890282ed65ab01bf89de386c89f
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-05-07 23:33:21 +0600

    Added the missing lessons

--- src/main/java/dev/farhan/springneo4j/controllers/CourseEnrollmentController.java
+++ src/main/java/dev/farhan/springneo4j/controllers/CourseEnrollmentController.java
@@ -2,10 +2,13 @@ package dev.farhan.springneo4j.controllers;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import dev.farhan.springneo4j.objects.CourseDTO;
 import dev.farhan.springneo4j.objects.CourseEnrolmentDTO;
 import dev.farhan.springneo4j.queryresults.CourseEnrolmentQueryResult;
 import dev.farhan.springneo4j.services.CourseEnrolmentService;
+import dev.farhan.springneo4j.services.LessonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +24,32 @@ import dev.farhan.springneo4j.requests.CourseEnrollmentRequest;
 @RequestMapping("/api/v1/enrollments")
 public class CourseEnrollmentController {
     private final CourseEnrolmentService courseEnrolmentService;
+    private final LessonService lessonService;
 
-    public CourseEnrollmentController(CourseEnrolmentService courseEnrolmentService) {
+    public CourseEnrollmentController(CourseEnrolmentService courseEnrolmentService, LessonService lessonService) {
         this.courseEnrolmentService = courseEnrolmentService;
+        this.lessonService = lessonService;
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Course>> enrollments(Principal principal) {
-        return new ResponseEntity<>(courseEnrolmentService.getAllEnrolledCoursesByUsername(principal.getName()), HttpStatus.OK);
+    public ResponseEntity<List<CourseDTO>> enrollments(Principal principal) {
+        List<Course> courses = courseEnrolmentService.getAllEnrolledCoursesByUsername(principal.getName());
+
+        List<CourseDTO> responseCourses = courses.stream().map(
+                (course) -> {
+                    CourseDTO responseCourse = new CourseDTO();
+
+                    responseCourse.setIdentifier(course.getIdentifier());
+                    responseCourse.setTitle(course.getTitle());
+                    responseCourse.setTeacher(course.getTeacher());
+                    responseCourse.setLessons(lessonService.getAllLessonsByCourseIdentifier(course.getIdentifier()));
+                    responseCourse.setEnrolled(true);
+
+                    return responseCourse;
+                }
+        ).collect(Collectors.toList());
+
+        return new ResponseEntity<>(responseCourses, HttpStatus.OK);
     }
 
     @PostMapping("/")
