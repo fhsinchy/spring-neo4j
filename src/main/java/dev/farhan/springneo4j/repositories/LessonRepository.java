7045cd1dd3e22fe6b989d9259c37bc37b205f819
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-05-07 22:51:09 +0600

    Added direction to lesson relationship

--- src/main/java/dev/farhan/springneo4j/repositories/LessonRepository.java
+++ src/main/java/dev/farhan/springneo4j/repositories/LessonRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.neo4j.repository.query.Query;
 import java.util.List;
 
 public interface LessonRepository extends Neo4jRepository<Lesson, Long> {
-    @Query("MATCH (:Course {identifier: $identifier})-[r:BELONGS_TO]-(lessons:Lesson) RETURN lessons")
+    @Query("MATCH (:Course {identifier: $identifier})<-[r:BELONGS_TO]-(lessons:Lesson) RETURN lessons")
     List<Lesson> findLessonsByCourseIdentifier(String identifier);
 }
