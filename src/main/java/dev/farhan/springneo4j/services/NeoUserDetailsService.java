c9cbcc67ea51a3ff8363662f468eabeec44c513c
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-05-20 22:26:02 +0600

    Syntactic updates

--- src/main/java/dev/farhan/springneo4j/services/NeoUserDetailsService.java
+++ src/main/java/dev/farhan/springneo4j/services/NeoUserDetailsService.java
@@ -1,7 +1,6 @@
 package dev.farhan.springneo4j.services;
 
 import dev.farhan.springneo4j.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
--- src/main/java/dev/farhan/springneo4j/services/UserService.java
+++ src/main/java/dev/farhan/springneo4j/services/UserService.java
@@ -1,17 +1,11 @@
 package dev.farhan.springneo4j.services;
 
-import dev.farhan.springneo4j.models.Course;
 import dev.farhan.springneo4j.models.User;
-import dev.farhan.springneo4j.queryresults.CourseEnrolmentQueryResult;
 import dev.farhan.springneo4j.repositories.UserRepository;
 import dev.farhan.springneo4j.requests.CreateUserRequest;
 
-import org.springframework.http.HttpStatusCode;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @Service
 public class UserService {
